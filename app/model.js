cfbb71cf20585119753a0d9fd18666e931211bb2
awwakum <awwakum@gmail.com>
2016-02-04 13:17:50 +0100

    edit model

--- app/model.js
+++ app/model.js
@@ -2,7 +2,6 @@ var shortid = require('shortid');
 var mongoose = require('mongoose');
 var credentials = require('../credentials');
 
-//mongoose.connect('mongodb://awwakum:url-shortener@ds033744.mongolab.com:33744/url-shortener');
 mongoose.connect('mongodb://'+ credentials.user + ':' + credentials.pass + '@ds033744.mongolab.com:33744/url-shortener');
 
 var urlSchema = new mongoose.Schema ({
